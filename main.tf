36f8d403f7452fe397e1884ea2104abac6350e61
Isen <3904043+isen-ng@users.noreply.github.com>
2019-06-10 12:06:04 +0800

    upgrade aws naming provider for 0.12 readyness (#11)

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "random_lc" {
-  source = "github.com/traveloka/terraform-aws-resource-naming.git?ref=v0.6.0"
+  source = "github.com/traveloka/terraform-aws-resource-naming.git?ref=v0.16.1"
 
   name_prefix   = "${var.service_name}-${var.cluster_role}"
   resource_type = "launch_configuration"
