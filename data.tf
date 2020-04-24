0a283b3911801132432c8bbf4cc362b4995b6360
Salvian Reynaldi <5778914+salvianreynaldi@users.noreply.github.com>
2020-04-24 11:50:10 +0700

    upgrade to 0.12 native syntax (#35)
    * upgrade to 0.12 native syntax
    
    * use smaller default instance
    
    * update list variables
    
    * fix image_filters type
    
    * fix image_filters type
    
    * applicable code

--- data.tf
+++ data.tf
@@ -1,8 +1,12 @@
 data "aws_ami" "latest_service_image" {
-  owners      = ["${var.image_owners}"]
+  owners      = var.image_owners
   most_recent = true
 
-  filter = [
-    "${var.image_filters}",
-  ]
+  dynamic "filter" {
+    for_each = var.image_filters
+    content {
+      name   = filter.value.name
+      values = filter.value.values
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,95 +1,95 @@
 variable "service_name" {
-  type        = "string"
+  type        = string
   description = "The name of the service"
 }
 
 variable "cluster_role" {
-  type        = "string"
+  type        = string
   default     = "app"
   description = "Primary role/function of the cluster"
 }
 
 variable "environment" {
-  type        = "string"
+  type        = string
   description = "The created resources will belong to this infrastructure environment"
 }
 
 variable "application" {
-  type        = "string"
+  type        = string
   description = "Application type that the ASG's instances will serve"
 }
 
 variable "product_domain" {
-  type        = "string"
+  type        = string
   description = "Abbreviation of the product domain this ASG and its instances belongs to"
 }
 
 variable "description" {
-  type        = "string"
+  type        = string
   description = "Free form description of this ASG and its instances"
 }
 
 variable "asg_vpc_zone_identifier" {
-  type        = "list"
+  type        = list(string)
   description = "The created ASG will spawn instances to these subnet IDs"
 }
 
 variable "asg_lb_target_group_arns" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "The created ASG will be attached to this target group"
 }
 
 variable "asg_clb_names" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "A list of classic load balancer names to add to the autoscaling group"
 }
 
 variable "asg_min_capacity" {
-  type        = "string"
+  type        = string
   default     = "0"
   description = "The created ASG will have this number of instances at minimum"
 }
 
 variable "asg_max_capacity" {
-  type        = "string"
+  type        = string
   default     = "0"
   description = "The created ASG will have this number of instances at maximum"
 }
 
 variable "asg_health_check_type" {
-  type        = "string"
+  type        = string
   default     = "ELB"
   description = "Controls how ASG health checking is done"
 }
 
 variable "asg_health_check_grace_period" {
-  type        = "string"
+  type        = string
   default     = "300"
   description = "Time, in seconds, to wait for new instances before checking their health"
 }
 
 variable "asg_default_cooldown" {
-  type        = "string"
+  type        = string
   default     = "300"
   description = "Time, in seconds, the minimum interval of two scaling activities"
 }
 
 variable "asg_placement_group" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "The placement group for the spawned instances"
 }
 
 variable "asg_metrics_granularity" {
-  type        = "string"
+  type        = string
   default     = "1Minute"
   description = "The granularity to associate with the metrics to collect"
 }
 
 variable "asg_enabled_metrics" {
-  type = "list"
+  type = list(string)
 
   default = [
     "GroupMinSize",
@@ -106,13 +106,13 @@ variable "asg_enabled_metrics" {
 }
 
 variable "asg_service_linked_role_arn" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "The ARN of the service-linked role that the ASG will use to call other AWS services"
 }
 
 variable "asg_termination_policies" {
-  type = "list"
+  type = list(string)
 
   default = [
     "Default",
@@ -122,31 +122,31 @@ variable "asg_termination_policies" {
 }
 
 variable "asg_tags" {
-  type        = "list"
+  type        = list(map(string))
   default     = []
   description = "The created ASG will have these tags applied over the default ones (see main.tf)"
 }
 
 variable "asg_wait_for_capacity_timeout" {
-  type        = "string"
+  type        = string
   description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out"
 }
 
 variable "asg_wait_for_elb_capacity" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Terraform will wait for exactly this number of healthy instances in all attached load balancers on both create and update operations. If left to default, the value is set to asg_min_capacity"
 }
 
 variable "launch_template_overrides" {
-  type = "list"
+  type = list(map(string))
 
   default = [
     {
-      "instance_type" = "c5.large"
+      "instance_type" = "c4.large"
     },
     {
-      "instance_type" = "c4.large"
+      "instance_type" = "t3.medium"
     },
   ]
 
@@ -154,64 +154,68 @@ variable "launch_template_overrides" {
 }
 
 variable "security_groups" {
-  type        = "list"
+  type        = list(string)
   description = "The spawned instances will have these security groups"
 }
 
 variable "instance_profile_name" {
-  type        = "string"
+  type        = string
   description = "The spawned instances will have this IAM profile"
 }
 
 variable "key_name" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "The spawned instances will have this SSH key name"
 }
 
 variable "cpu_credits" {
-  type        = "string"
+  type        = string
   default     = "unlimited"
   description = "The credit option for CPU usage, can be either 'standard' or 'unlimited'"
 }
 
 variable "image_filters" {
-  type        = "list"
+  type = list(object({
+    name   = string,
+    values = list(string)
+    })
+  )
   description = "The AMI search filters. The most recent AMI that pass this filter will be deployed to the ASG. See https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-images.html"
 }
 
 variable "image_owners" {
-  type        = "list"
+  type        = list(string)
   description = "List of AMI owners to limit search. This becomes required starting terraform-provider-aws v2 (https://www.terraform.io/docs/providers/aws/guides/version-2-upgrade.html#owners-argument-now-required)"
 }
 
 variable "monitoring" {
-  type        = "string"
+  type        = string
   default     = "true"
   description = "The spawned instances will have enhanced monitoring if enabled"
 }
 
 variable "ebs_optimized" {
-  type        = "string"
+  type        = string
   default     = "false"
   description = "The spawned instances will have EBS optimization if enabled"
 }
 
 variable "user_data" {
-  type        = "string"
+  type        = string
   default     = " "
   description = "The spawned instances will have this user data. Use the rendered value of a terraform's `template_cloudinit_config` data" // https://www.terraform.io/docs/providers/template/d/cloudinit_config.html#rendered
 }
 
 variable "volume_size" {
   description = "The size of the volume in gigabytes"
-  type        = "string"
+  type        = string
   default     = "8"
 }
 
 variable "volume_type" {
   description = "The type of volume. Can be standard, gp2, or io1"
-  type        = "string"
+  type        = string
   default     = "gp2"
 }
 
@@ -231,7 +235,7 @@ variable "associate_public_ip" {
 }
 
 variable "mixed_instances_distribution" {
-  type        = "map"
+  type        = map(string)
   description = "Specify the distribution of on-demand instances and spot instances. See https://docs.aws.amazon.com/autoscaling/ec2/APIReference/API_InstancesDistribution.html"
 
   default = {
